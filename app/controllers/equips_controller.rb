48fd4533753b54878744f60852c21831fdd30548
Steven Mickelson <caniac@Stevens-MacBook-Air.local>
2022-02-19 21:48:58 -0600

    updating equip controller

--- app/controllers/equips_controller.rb
+++ app/controllers/equips_controller.rb
@@ -8,9 +8,5 @@ class EquipsController < ApplicationController
     private
         def equip_params
             params.require(:equip.permit(:heml, :armor, :accessory, :weapon)
-            helm = params[:helm].presence || "none"
-            armor = params[:armor].presence || "none"
-            accessory = params[:accessory].presence || "none"
-            weapon = params[:weapon].presence || "none"
         end
 end
