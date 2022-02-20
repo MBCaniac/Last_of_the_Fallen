c0fa07b4723dae542add44cf83a578dd9fd20b5b
Steven Mickelson <caniac@Stevens-MacBook-Air.local>
2022-02-20 15:17:42 -0600

    remove update_defaults from update

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -8,7 +8,6 @@ class Unit < ApplicationRecord
     validates :gender, presence: true
 
     after_create :update_defaults
-    after_update :update_defaults
     
     def update_defaults
         update(char_lvl: 1)
