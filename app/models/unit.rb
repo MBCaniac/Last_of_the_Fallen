d6272b1f1bb7b7b679ea7ae2c26183fc5c969071
Steven Mickelson <caniac@Stevens-MacBook-Air.local>
2022-03-07 15:58:53 -0600

    testing if order matters

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -8,12 +8,12 @@ class Unit < ApplicationRecord
     after_create :update_defaults
     
     def update_defaults
-       if (self.classtype = "warrior")
-        update(char_lvl: 1, char_xp: 0, char_atp: 30, char_def: 50, char_eva: 30, char_hp: 100, char_tp: 10)
-       elsif (self.classtype = "ranger")
-        update(char_lvl: 1, char_xp: 0, char_atp: 40, char_def: 30, char_eva: 40, char_hp: 80, char_tp: 20)
-       else
-        update(char_lvl: 1, char_xp: 0, char_atp: 50, char_def: 20, char_eva: 40, char_hp: 60, char_tp: 30)
-       end
+        if (self.classtype = "ranger")
+            update(char_lvl: 1, char_xp: 0, char_atp: 40, char_def: 30, char_eva: 40, char_hp: 80, char_tp: 20)
+        elsif (self.classtype = "warrior")
+            update(char_lvl: 1, char_xp: 0, char_atp: 30, char_def: 50, char_eva: 30, char_hp: 100, char_tp: 10)
+        else
+            update(char_lvl: 1, char_xp: 0, char_atp: 50, char_def: 20, char_eva: 40, char_hp: 60, char_tp: 30)
+        end
     end
 end
