d1da3a422afeaae68aa23d2e3d3e70f025eb182a
Steven Mickelson <caniac@Stevens-MacBook-Air.local>
2022-03-07 15:44:14 -0600

    made update defaults more efficient

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -8,26 +8,12 @@ class Unit < ApplicationRecord
     after_create :update_defaults
     
     def update_defaults
-        update(char_lvl: 1)
-        update(char_xp: 0)
        if (self.classtype = "warrior")
-        update(char_atp: 30)
-        update(char_def: 50)
-        update(char_eva: 30)
-        update(char_hp: 100)
-        update(char_tp: 10)
+        update(char_lvl: 1, char_xp: 0, char_atp: 30, char_def: 50, char_eva: 30, char_hp: 100, char_tp: 10)
        elsif (self.classtype = "ranger")
-        update(char_atp: 40)
-        update(char_def: 30)
-        update(char_eva: 40)
-        update(char_hp: 80)
-        update(char_tp: 20)
+        update(char_lvl: 1, char_xp: 0, char_atp: 40, char_def: 30, char_eva: 40, char_hp: 80, char_tp: 20)
        else
-        update(char_atp: 50)
-        update(char_def: 20)
-        update(char_eva: 40)
-        update(char_hp: 60)
-        update(char_tp: 30)
+        update(char_lvl: 1, char_xp: 0, char_atp: 50, char_def: 20, char_eva: 40, char_hp: 60, char_tp: 30)
        end
     end
 end
