15fe482b93e8be5d27e1e1e76bf161498444b1a8
Steven Mickelson <caniac@Stevens-MacBook-Air.local>
2022-03-07 16:01:11 -0600

    adjust unit with correct string comparison

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -8,9 +8,9 @@ class Unit < ApplicationRecord
     after_create :update_defaults
     
     def update_defaults
-        if (self.classtype = "ranger")
+        if (self.classtype == "ranger")
             update(char_lvl: 1, char_xp: 0, char_atp: 40, char_def: 30, char_eva: 40, char_hp: 80, char_tp: 20)
-        elsif (self.classtype = "warrior")
+        elsif (self.classtype == "warrior")
             update(char_lvl: 1, char_xp: 0, char_atp: 30, char_def: 50, char_eva: 30, char_hp: 100, char_tp: 10)
         else
             update(char_lvl: 1, char_xp: 0, char_atp: 50, char_def: 20, char_eva: 40, char_hp: 60, char_tp: 30)
